391eebef0328a477a992134c6e9eac9d2a498114
sunitha1238@gmail.com <sunitha1238@gmail.com>
2021-04-22 16:50:42 +0530

    Adding entity classes for reservation and payment services

---
+++ reservationservice/src/main/java/com/epam/reservationservice/controller/ReservationServiceControllerInterface.java
@@ -0,0 +1,21 @@
+package com.epam.reservationservice.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/v1/api")
+public interface ReservationServiceControllerInterface {
+
+	@PostMapping("/reservations")
+	public String addReservation();
+	
+	@GetMapping("/reservations/{reservationId}")
+	public String getReservationDetails(@PathVariable int reservationId);
+	
+	@DeleteMapping("/reservations/{reservationId}")
+	public String CancelReservation(@PathVariable int reservationId);
+
+}
--- reservationservice/src/test/java/com/epam/reservationservice/ReservationserviceApplicationTests.java
+++ reservationservice/src/test/java/com/epam/reservationservice/ReservationserviceApplicationTests.java
@@ -2,12 +2,21 @@ package com.epam.reservationservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
-@SpringBootTest
+import com.epam.reservationservice.entity.ReservationDetails;
+
+
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class ReservationserviceApplicationTests {
 
+	TestRestTemplate restTemplate = new TestRestTemplate();
+	
 	@Test
 	void contextLoads() {
+		restTemplate.getForEntity("/v1/api/reservations/1", String.class);
 	}
 
 }
