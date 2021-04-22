21bdf2769506c509b0d7d2aaeb912d27b2b6800c
dinesh.sunku9@gmail.com <dinesh.sunku9@gmail.com>
2021-04-22 16:35:19 +0530

    Adding Test cases and Model calsses

--- guestservice/src/main/java/com/epam/guestservice/controller/GuestServiceController.java
+++ guestservice/src/main/java/com/epam/guestservice/controller/GuestServiceController.java
@@ -1,37 +1,45 @@
 package com.epam.guestservice.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.epam.guestservice.model.User;
+import com.epam.guestservice.service.GuestServiceClass;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
 public class GuestServiceController implements GuestServiceControllerInterface {
+
+	@Autowired
+	private GuestServiceClass guestServiceClass;
+
 	@Override
 	public User addUser(User user) {
 		log.info("Adding the user");
-		return user;
+		return guestServiceClass.addUser(user);
 	}
 
 	@Override
-	public String getUsers() {
-		return "All Users";
+	public List<User> getUsers() {
+		return guestServiceClass.getUsers();
 	}
 
 	@Override
-	public String getUserById(int userId) {
-		return "User is" + userId;
+	public User getUserById(int userid) {
+		return guestServiceClass.getUserById(userid);
 	}
 
 	@Override
 	public String updateUser(User user, int userId) {
-		return "User updated";
+		return guestServiceClass.updateUser(user, userId);
 	}
 
 	@Override
 	public String deleteUser(int userId) {
-		return "User updated";
+		return guestServiceClass.deleteUser(userId);
 	}
 }
