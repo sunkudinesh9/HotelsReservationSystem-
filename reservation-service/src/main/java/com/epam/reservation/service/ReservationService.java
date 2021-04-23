20588cc7c00eaa696c7a102422880ed9f1387c21
Sunitha_Kristipati <Sunitha_Kristipati@EPINBANW00DC>
2021-04-23 19:06:52 +0530

    Test cases for Reservation and Payment Controllers

--- reservation-service/src/main/java/com/epam/reservation/service/ReservationService.java
+++ reservation-service/src/main/java/com/epam/reservation/service/ReservationService.java
@@ -1,5 +1,23 @@
 package com.epam.reservation.service;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import com.epam.reservation.model.Reservation;
+
 public class ReservationService {
+
+	public Reservation addReservation(Reservation reservation) {
+		return reservation;
+	}
+
+	public Reservation getReservationDetailsById(int anyInt) {
+		return new Reservation();
+	}
+
+	public List<Reservation> getAllReservations() {
+		return Collections.emptyList();
+	}
 
 }
