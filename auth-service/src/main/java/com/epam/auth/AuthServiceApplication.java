9ff016b105da483df3a96f227be5a1d4aebe801a
Dinesh Sunku <dinesh_sunku@epam.com>
2021-04-30 19:23:03 +0530

    Adding Auth-service and Gateway-service

--- auth-service/src/main/java/com/epam/auth/AuthServiceApplication.java
+++ auth-service/src/main/java/com/epam/auth/AuthServiceApplication.java
@@ -1,17 +1,17 @@
-package com.epam.springcloudconfig;
+package com.epam.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableConfigServer
+@EnableFeignClients
 @EnableEurekaClient
-public class SpringCloudConfigServerApplication {
+public class AuthServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+		SpringApplication.run(AuthServiceApplication.class, args);
 	}
 
 }
