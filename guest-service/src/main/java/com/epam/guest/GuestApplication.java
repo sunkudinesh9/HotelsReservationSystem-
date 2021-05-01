3c50bb894e6e02cadd3e64a7ec57f71a7944508c
Anuj_Kumar2 <Anuj_Kumar2@EPINPUNW0167>
2021-05-01 22:34:59 +0530

    Enabled Eureka

--- guest-service/src/main/java/com/epam/guest/GuestApplication.java
+++ guest-service/src/main/java/com/epam/guest/GuestApplication.java
@@ -3,7 +3,6 @@ package com.epam.guest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
